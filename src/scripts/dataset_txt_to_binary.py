a3b703bf0b3e41cf9664cbe527ac97109e35d408
Hugo Phibbs <hugophibbs1@gmail.com>
2025-04-11 16:48:37 +1200

    changing directories to convert txt datasets to binary

--- src/scripts/dataset_txt_to_binary.py
+++ src/scripts/dataset_txt_to_binary.py
@@ -23,31 +23,31 @@ def dataset_txt_to_binary(input_file, output_file):
     print(f"Wrote {n}x{d} data to {output_file}")
 
 if __name__ == "__main__":
-    # txt_filenames = [
-    #     "/workspace/CUDA-CEOs/datasets-ninh/imagenet/_Q_200_150.txt",
-    #     "/workspace/CUDA-CEOs/datasets-ninh/imagenet/_X_2340373_150.txt",
-    #     "/workspace/CUDA-CEOs/datasets-ninh/msong/_Q_1000_420.txt",
-    #     "/workspace/CUDA-CEOs/datasets-ninh/msong/_X_994185_420.txt",
-    #     "/workspace/CUDA-CEOs/datasets-ninh/nuswide/_Q_200_500.txt",
-    #     "/workspace/CUDA-CEOs/datasets-ninh/nuswide/_X_268643_500.txt",
-    #     "/workspace/CUDA-CEOs/datasets-ninh/yahoo/Yahoo_Q_1000_300.txt",
-    #     "/workspace/CUDA-CEOs/datasets-ninh/yahoo/Yahoo_X_624961_300.txt"
-    # ]
+    txt_filenames = [
+        "/workspace/CUDA-CEOs/datasets/txt/imagenet/_Q_200_150.txt",
+        "/workspace/CUDA-CEOs/datasets/txt/imagenet/_X_2340373_150.txt",
+        "/workspace/CUDA-CEOs/datasets/txt/msong/_Q_1000_420.txt",
+        "/workspace/CUDA-CEOs/datasets/txt/msong/_X_994185_420.txt",
+        "/workspace/CUDA-CEOs/datasets/txt/nuswide/_Q_200_500.txt",
+        "/workspace/CUDA-CEOs/datasets/txt/nuswide/_X_268643_500.txt",
+        "/workspace/CUDA-CEOs/datasets/txt/yahoo/Yahoo_Q_1000_300.txt",
+        "/workspace/CUDA-CEOs/datasets/txt/yahoo/Yahoo_X_624961_300.txt"
+    ]
 
-    # bin_filenames = [
-    #     "/workspace/CUDA-CEOs/datasets/imagenet/imagenet_queries.bin",
-    #     "/workspace/CUDA-CEOs/datasets/imagenet/imagenet_dataset.bin",
-    #     "/workspace/CUDA-CEOs/datasets/msong/msong_queries.bin",
-    #     "/workspace/CUDA-CEOs/datasets/msong/msong_dataset.bin",
-    #     "/workspace/CUDA-CEOs/datasets/nuswide/nuswide_queries.bin",
-    #     "/workspace/CUDA-CEOs/datasets/nuswide/nuswide_dataset.bin",
-    #     "/workspace/CUDA-CEOs/datasets/yahoo/yahoo_queries.bin",
-    #     "/workspace/CUDA-CEOs/datasets/yahoo/yahoo_dataset.bin"
-    # ]
+    bin_filenames = [
+        "/workspace/CUDA-CEOs/datasets/bin/imagenet/imagenet_queries.bin",
+        "/workspace/CUDA-CEOs/datasets/bin/imagenet/imagenet_dataset.bin",
+        "/workspace/CUDA-CEOs/datasets/bin/msong/msong_queries.bin",
+        "/workspace/CUDA-CEOs/datasets/bin/msong/msong_dataset.bin",
+        "/workspace/CUDA-CEOs/datasets/bin/nuswide/nuswide_queries.bin",
+        "/workspace/CUDA-CEOs/datasets/bin/nuswide/nuswide_dataset.bin",
+        "/workspace/CUDA-CEOs/datasets/bin/yahoo/yahoo_queries.bin",
+        "/workspace/CUDA-CEOs/datasets/bin/yahoo/yahoo_dataset.bin"
+    ]
 
-    # for txt_filename, bin_filename in tqdm(zip(txt_filenames, bin_filenames), "Writing files"):
-    #     dataset_txt_to_binary(txt_filename, bin_filename)
+    for txt_filename, bin_filename in tqdm(zip(txt_filenames, bin_filenames), "Writing files"):
+        dataset_txt_to_binary(txt_filename, bin_filename)
 
-    array = np.fromfile("/workspace/CUDA-CEOs/datasets/imagenet/imagenet_dataset.bin", dtype=np.float32)
-    array = array.reshape(-1, 150)
-    print(array.shape)
+    # array = np.fromfile("/workspace/CUDA-CEOs/datasets/imagenet/imagenet_dataset.bin", dtype=np.float32)
+    # array = array.reshape(-1, 150)
+    # print(array.shape)
